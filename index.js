0a1f5b78f2ba469cbc20b3a06340fbc2ac89fa90
Noah Isaacson <clewfirst@gmail.com>
2013-05-02 17:41:23 -0400

    added travis.yml

---
+++ index.js
@@ -0,0 +1,56 @@
+var inspect = require('eyespect').inspector();
+
+var rk = require('required-keys');
+var getSpawnCommandID = require('./lib/getSpawnCommandID')
+module.exports = function (data, cb) {
+  var keys = ['repo', 'command', 'instances', 'db']
+  var err = rk.truthySync(data, keys)
+  if (err) {
+    return cb({
+      message: 'failed to add command, missing key in data',
+      error: err,
+      stack: new Error().stack
+    })
+  }
+
+  var repo = data.repo
+  var command = data.command
+  var drone = data.drone || ''
+  var instances = data.instances
+  var db = data.db
+  var id = getSpawnCommandID(data)
+  db.get(id, function (err, reply) {
+    if (reply) {
+      return cb({
+        message: 'failed to add spawn command',
+        error: 'spawn command already exists',
+        stack: new Error().stack
+      })
+    }
+
+    if (err && err.error !== 'not_found') {
+      return cb({
+        message: 'failed to add spawn command',
+        error: err,
+        stack: new Error().stack
+      })
+    }
+    var record = {
+      repo: repo,
+      command: command,
+      drone: drone,
+      instances: instances,
+      resource: 'SpawnCommand'
+    }
+    db.save(id, record, function (err, reply) {
+      if (err) {
+        return cb({
+          message: 'failed to add spawn command, error when saving to the database',
+          error: err,
+          stack: new Error().stack
+        })
+      }
+      cb()
+    })
+  })
+}
